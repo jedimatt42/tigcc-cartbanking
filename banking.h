a7951aa481e1195572a4c302025a8c0336c39100
Matthew Splett <matt@cwfk.net>
2019-01-13 11:15:57 -0800

    correct casting warnings

--- banking.h
+++ banking.h
@@ -51,7 +51,7 @@ __attribute__ ((gnu_inline, always_inline)) \
 static inline return_type banked_name param_types { \
   static const int foo[]={(int)MYBANK, (int)realname, (int)bank}; \
   tramp_data = (int*) foo; \
-  return trampoline param_list; \
+  return (return_type) trampoline param_list; \
 }
 
 #define DECLARE_BANKED_VOID(realname, bank, banked_name, param_types, param_list) \
@@ -67,7 +67,7 @@ __attribute__ ((gnu_inline, always_inline)) \
 static inline return_type banked_name param_types { \
   static const int foo[]={(int)MYBANK, (int)realname, (int)bank}; \
   tramp_data = (int*) foo; \
-  return ((int)(trampoline param_list)) >> 8; \
+  return (return_type) (((int)(trampoline param_list)) >> 8); \
 }
 
 
